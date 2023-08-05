9d4de2736d65c6b095ecfee36c9749b8ea265762
Amitha K <amithakumar429@gmail.com>
2023-08-05 09:15:07 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import image from'./celeberate_womentech.png'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    return (
+    <div>
+        <div>
+           <p id="hello">I am in 3rd year.</p>
+           <button type="button" onclick="myFunfor1st()">1st</button>
+           <button type="button" onclick="myFunfor2nd()">2nd</button>
+           <button type="button" onclick="myFunfor3rd()">3rd</button>
+           <button type="button" onclick="myFunfor4th()">4th</button>
+           <p> Hii..I am Amitha.I am pursuing <span>B.tech-Artificial Intelligence and Data Science department</span> in Kumaraguru college of technology</p>
+           <img src={image}></img>
+         
+        
+        <h2><b><i>Hobbies</i></b></h2>
+           <p>I like to watch cricket.I like singing. I like to play carrom</p>
+        
+        </div>
+        <div>
+        <h3><b><i>Favourite Subject</i></b></h3>
+           <ul>
+              <li>Maths</li>
+              <li>Machine Learning</li>
+              <li>Data Science</li>
+           </ul>
+          
+        </div> 
+        <div>
+        <h4><b><i>Language Known</i></b></h4>
+            <ol type="A">
+                <li>Tamil</li>
+                <li>English</li>
+            </ol>
+        
+        <h4><b><i>Skills</i></b></h4>
+            <ol type="I">
+                <li>Python</li>
+                <li>Machine Learning</li>
+                <li>SQL</li>
+                <li>HTML and CSS</li>
+            </ol>
+        </div>
+        
+        <div class="grid-container">
+             <div class="grid-item">Artificial Intelligence</div>
+             <div class="grid-item">Data science</div>
+        </div>
+        <div class="flex-container">
+            <div>HTML</div>
+            <div>CSS</div>
+        </div>
+        <script src="JS/linkForHtml.js"></script>
     </div>
-  );
-}
+    )
+};
 
 export default App;
