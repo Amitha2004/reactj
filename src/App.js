ed805849f3241e5cc9a4e75383fa380f68ef4429
Amitha K <amithakumar429@gmail.com>
2023-08-05 10:40:10 +0530

    2

--- src/App.js
+++ src/App.js
@@ -6,18 +6,14 @@ function App() {
     return (
     <div>
         <div>
-           <p id="hello">I am in 3rd year.</p>
-           <button type="button" onclick="myFunfor1st()">1st</button>
-           <button type="button" onclick="myFunfor2nd()">2nd</button>
-           <button type="button" onclick="myFunfor3rd()">3rd</button>
-           <button type="button" onclick="myFunfor4th()">4th</button>
+           <h1><b><i>About me</i></b></h1>
            <p> Hii..I am Amitha.I am pursuing <span>B.tech-Artificial Intelligence and Data Science department</span> in Kumaraguru college of technology</p>
            <img src={image}></img>
          
-        
+        <hr />
         <h2><b><i>Hobbies</i></b></h2>
            <p>I like to watch cricket.I like singing. I like to play carrom</p>
-        
+        <hr />
         </div>
         <div>
         <h3><b><i>Favourite Subject</i></b></h3>
@@ -26,7 +22,7 @@ function App() {
               <li>Machine Learning</li>
               <li>Data Science</li>
            </ul>
-          
+        <hr />  
         </div> 
         <div>
         <h4><b><i>Language Known</i></b></h4>
@@ -34,7 +30,7 @@ function App() {
                 <li>Tamil</li>
                 <li>English</li>
             </ol>
-        
+        <hr />
         <h4><b><i>Skills</i></b></h4>
             <ol type="I">
                 <li>Python</li>
@@ -43,11 +39,12 @@ function App() {
                 <li>HTML and CSS</li>
             </ol>
         </div>
-        
+        <hr />
         <div class="grid-container">
              <div class="grid-item">Artificial Intelligence</div>
              <div class="grid-item">Data science</div>
         </div>
+        <br />
         <div class="flex-container">
             <div>HTML</div>
             <div>CSS</div>
